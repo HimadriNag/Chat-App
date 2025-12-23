b52457853ab741447f2fcb107d6ab2fd1a9c7bf4
HimadriNag <himadrinag92@gmail.com>
2025-12-23 23:12:38 +0530

    auth logic

--- forntend/src/App.jsx
+++ forntend/src/App.jsx
@@ -1,22 +1,32 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { Toaster } from 'react-hot-toast';
 import Home from './pages/Home';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 import Profile from './pages/Profile';
 import Navbar from './components/Navbar';
+import { useAuthStore } from './store/useAuthStore';
+import {Loader} from "lucide-react";
 
 const App = () => {
+  const {authUser,checkAuth,isCheckingAuth}=useAuthStore();
+  useEffect(()=>{
+    checkAuth();
+  },[checkAuth]);
+  if (isCheckingAuth && !authUser)
+    return <div className='flex items-center justify-center h-screen'>
+      <Loader className='size-10n animate-spin text-white'/>
+    </div>
   return (
     <div>
       <Toaster />
       <Navbar/>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/profile' element={<Profile />} />
+        <Route path='/' element={authUser?<Home />:<Navigate to="/login"/>} />
+        <Route path='/signup' element={!authUser?<Signup />:<Navigate to="/"/>}/>
+        <Route path='/login' element={!authUser?<Login />:<Navigate to="/"/>} />
+        <Route path='/profile' element={authUser? <Profile />:<Navigate to="/login"/>} />
       </Routes>
     </div>
   )
--- forntend/src/store/useAuthStore.js
+++ forntend/src/store/useAuthStore.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import {create} from "zustand";
 
 
@@ -6,5 +7,22 @@ export const useAuthStore=create((set)=>({
     isSigningUp:false,
     isLoggingIn:false,
     isCheckingAuth:true,
+
+    checkAuth:async()=>{
+    try {
+        const {data} =await axios.get("/users/check");
+        set({authUser:data});
+        
+    } catch (error) {
+        console.log("error in checkAuth",error);
+        set({authUser:null});
+        
+    }finally{
+        set({isCheckingAuth:false});
+    }
+}
+
+    
     
 }));
+
