585d648227fc612764684b5a52a8e4f3ebbfc3af
HimadriNag <himadrinag92@gmail.com>
2025-12-24 23:36:31 +0530

    Forntend Sign-Up logic

--- forntend/src/App.jsx
+++ forntend/src/App.jsx
@@ -7,29 +7,40 @@ import Login from './pages/Login';
 import Profile from './pages/Profile';
 import Navbar from './components/Navbar';
 import { useAuthStore } from './store/useAuthStore';
-import {Loader} from "lucide-react";
+import { Loader } from "lucide-react";
+
 
 const App = () => {
-  const {authUser,checkAuth,isCheckingAuth}=useAuthStore();
-  useEffect(()=>{
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
+  useEffect(() => {
     checkAuth();
-  },[checkAuth]);
-  if (isCheckingAuth && !authUser)
-    return <div className='flex items-center justify-center h-screen'>
-      <Loader className='size-10n animate-spin text-white'/>
-    </div>
+  }, [checkAuth]);
+  // App.jsx
+  if (isCheckingAuth && !authUser) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+  }
+
   return (
     <div>
-      <Toaster />
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={authUser?<Home />:<Navigate to="/login"/>} />
-        <Route path='/signup' element={!authUser?<Signup />:<Navigate to="/"/>}/>
-        <Route path='/login' element={!authUser?<Login />:<Navigate to="/"/>} />
-        <Route path='/profile' element={authUser? <Profile />:<Navigate to="/login"/>} />
-      </Routes>
-    </div>
-  )
+    <Navbar />
+    <Toaster />
+    <Routes>
+    
+      <Route path="/" element={authUser ? <Home /> : <Navigate to="/login" />} />
+
+      
+      <Route path="/signup" element={!authUser ? <Signup /> : <Navigate to="/" />} />
+      <Route path="/login" element={!authUser ? <Login /> : <Navigate to="/" />} />
+
+      <Route path="/profile" element={authUser ? <Profile /> : <Navigate to="/login" />} />
+    </Routes>
+     </div>
+  );
+
 }
 
 export default App
--- forntend/src/store/useAuthStore.js
+++ forntend/src/store/useAuthStore.js
@@ -1,28 +1,48 @@
-import axios from "axios";
-import {create} from "zustand";
+import {axiosInstance} from "../lib/axios"
+import { create } from "zustand";
+import toast from "react-hot-toast";
 
+export const useAuthStore = create((set) => ({
+    authUser: null,
+    isSigningUp: false,
+    isLoggingIn: false,
+    isCheckingAuth: true,
 
-export const useAuthStore=create((set)=>({
-    authUser:null,
-    isSigningUp:false,
-    isLoggingIn:false,
-    isCheckingAuth:true,
+  
+    checkAuth: async () => {
+        try {
+            const res = await axiosInstance.get("/users/check");
+           
+            if (res.data) {
+                set({ authUser: res.data });
+            } else {
+                set({ authUser: null });
+            }
+        } catch (error) {
+            console.log("error in checkAuth:", error);
+            set({ authUser: null }); 
+        } finally {
+            set({ isCheckingAuth: false });
+        }
+    },
 
-    checkAuth:async()=>{
-    try {
-        const {data} =await axios.get("/users/check");
-        set({authUser:data});
-        
-    } catch (error) {
-        console.log("error in checkAuth",error);
-        set({authUser:null});
-        
-    }finally{
-        set({isCheckingAuth:false});
+    signup: async (data) => {
+        set({ isSigningUp: true });
+        try {
+            const res = await axiosInstance.post("/users/signup", data);
+            set({ authUser: res.data });
+            toast.success(res.data.message);
+
+        } catch (error) {
+            console.log("error in signup", error);
+            toast.success(error.response.data.message)
+
+        } finally {
+            set({ isSigningUp: false });
+        }
     }
-}
 
-    
-    
+
+
 }));
 
