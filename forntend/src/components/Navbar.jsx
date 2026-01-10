4b017c3301c0663aa0410c0795ea87535b82b581
HimadriNag <himadrinag92@gmail.com>
2026-01-10 10:51:40 +0530

    No Chat selected logic

--- forntend/src/components/Navbar.jsx
+++ forntend/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { useAuthStore } from '../store/useAuthStore';
 
 const Navbar = () => {
-  const {logout}=useAuthStore();
+  const {logout,authUser}=useAuthStore();
   return (
     <div className="navbar bg-base-100 shadow-sm">
       <div className="flex-1">
@@ -18,7 +18,8 @@ const Navbar = () => {
             <div className="w-10 rounded-full">
               <img
                 alt="Tailwind CSS Navbar component"
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                src={authUser?.profilePic || 
+                "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"} />
             </div>
           </div>
           <ul
